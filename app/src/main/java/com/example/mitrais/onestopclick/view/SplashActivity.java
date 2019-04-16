b0a3448eb68b3cfa57ffa144b00db8d8d3cacce4
Zulfikar_M <zulfikar313@gmail.com>
2019-04-16 09:39:53 +0700

    Remove progress bar from splash screen

--- app/src/main/java/com/example/mitrais/onestopclick/view/SplashActivity.java
+++ app/src/main/java/com/example/mitrais/onestopclick/view/SplashActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
 
 import com.example.mitrais.onestopclick.App;
 import com.example.mitrais.onestopclick.Constant;
@@ -17,7 +15,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 import javax.inject.Inject;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import maes.tech.intentanim.CustomIntent;
 
@@ -29,9 +26,6 @@ public class SplashActivity extends AppCompatActivity {
 
     @Inject
     SplashViewModel viewModel;
-
-    @BindView(R.id.progress_bar)
-    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,10 +72,7 @@ public class SplashActivity extends AppCompatActivity {
      * start LoginActivity
      */
     private void goToLoginPage() {
-        showProgressBar();
         new Handler().postDelayed(() -> {
-            hideProgressBar();
-
             finish();
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
@@ -93,11 +84,8 @@ public class SplashActivity extends AppCompatActivity {
      * start MainActivity
      */
     private void goToMainPage() {
-        showProgressBar();
         viewModel.syncData(user)
                 .addOnCompleteListener(task -> {
-                    hideProgressBar();
-
                     finish();
                     Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
@@ -109,28 +97,11 @@ public class SplashActivity extends AppCompatActivity {
      * start RegistrationActivity
      */
     private void goToRegistrationPage() {
-        showProgressBar();
         new Handler().postDelayed(() -> {
-            hideProgressBar();
-
             finish();
             Intent intent = new Intent(SplashActivity.this, RegistrationActivity.class);
             startActivity(intent);
             CustomIntent.customType(SplashActivity.this, Constant.ANIMATION_FADEIN_TO_FADEOUT);
         }, Constant.PROGRESS_DELAY);
-    }
-
-    /**
-     * set progress bar visible
-     */
-    private void showProgressBar() {
-        progressBar.setVisibility(View.VISIBLE);
-    }
-
-    /**
-     * set progress bar invisible
-     */
-    private void hideProgressBar() {
-        progressBar.setVisibility(View.INVISIBLE);
     }
 }
