ea06ee5d198c7e4992859c008d576132493d247d
Zulfikar_M <zulfikar313@gmail.com>
2019-05-15 10:12:28 +0700

    Refactor all dao modules to one module class

--- app/src/main/java/com/example/mitrais/onestopclick/dagger/component/ProfileRepositoryComponent.java
+++ app/src/main/java/com/example/mitrais/onestopclick/dagger/component/ProfileRepositoryComponent.java
@@ -2,14 +2,14 @@ package com.example.mitrais.onestopclick.dagger.component;
 
 import android.app.Application;
 
-import com.example.mitrais.onestopclick.dagger.module.ProfileDaoModule;
+import com.example.mitrais.onestopclick.dagger.module.DaoModule;
 import com.example.mitrais.onestopclick.dagger.module.ServiceModule;
 import com.example.mitrais.onestopclick.model.repository.ProfileRepository;
 
 import dagger.BindsInstance;
 import dagger.Component;
 
-@Component(modules = {ServiceModule.class, ProfileDaoModule.class})
+@Component(modules = {ServiceModule.class, DaoModule.class})
 public interface ProfileRepositoryComponent {
     void inject(ProfileRepository profileRepository);
 
--- app/src/main/java/com/example/mitrais/onestopclick/dagger/module/DaoModule.java
+++ app/src/main/java/com/example/mitrais/onestopclick/dagger/module/DaoModule.java
@@ -3,13 +3,20 @@ package com.example.mitrais.onestopclick.dagger.module;
 import android.app.Application;
 
 import com.example.mitrais.onestopclick.model.room.MainDatabase;
+import com.example.mitrais.onestopclick.model.room.ProductDao;
 import com.example.mitrais.onestopclick.model.room.ProfileDao;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
-public class ProfileDaoModule {
+public class DaoModule {
+    @Provides
+    ProductDao provideProductDao(Application application) {
+        MainDatabase db = MainDatabase.getInstance(application);
+        return db.productDao();
+    }
+
     @Provides
     ProfileDao provideProfileDao(Application application) {
         MainDatabase db = MainDatabase.getInstance(application);
